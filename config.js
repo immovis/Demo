ecd0e1d3c25d55991fe1c7e20b2abf5f36a8f87e
Keyframe <58492865+ArchitecturalVisualisation@users.noreply.github.com>
2020-07-07 10:08:12 +0200

    Demo
    360°

---
+++ config.js
@@ -0,0 +1,11 @@
+/*
+* Theasys
+* Generated on: Sunday 24th of May 2020 11:14:15 AM
+* Config file: You can change the path to match your server path.
+*/
+
+var vars = {
+    file : 'index.html',
+    path : 'https://immovis.github.io/test',//path to the index.html
+    theme : 'theasys',
+};
